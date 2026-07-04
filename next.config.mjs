95486fc9bb2098fc42a1795a6a629abe492a61e4
Pxcheco <pachecofabian764@gmail.com>
2026-07-04 11:59:45 -0400

    Configure static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+const repoName = "music-split-calculator";
+
 const nextConfig = {
   reactStrictMode: true,
-  poweredByHeader: false
+  poweredByHeader: false,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: isGithubPages ? `/${repoName}` : "",
+  assetPrefix: isGithubPages ? `/${repoName}/` : ""
 };
 
 export default nextConfig;
